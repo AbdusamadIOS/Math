05ba61017429c8019d617c27a92776f8c3567dca
abdusamad <abdusamad2609@gmail.com>
2023-05-01 23:59:08 +0500

    Initial Commit

---
+++ Kvadrat chiqarish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kvadrat chiqarish
+//
+//  Created by Abdusamad Mamasoliyev on 01/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
